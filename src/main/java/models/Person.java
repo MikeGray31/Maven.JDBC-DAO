5510818893f6e5401aedd3f377dec242086e083e
MikeGray31 <michaelkgray31@gmail.com>
2019-12-07 15:28:33 -0500

    Update

---
+++ src/main/java/models/Person.java
@@ -0,0 +1,73 @@
+package models;
+
+public class Person {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String gender;
+
+    // constructors ---------------------------------------------
+
+    public Person() {
+    }
+
+    public Person(String firstName, String lastName, String email, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    public Person(Integer id, String firstName, String lastName, String email, String gender) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.gender = gender;
+    }
+
+    // getters and setters ----------------------------------------
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
